8de341af69f411954100274770f25565afda53ed
Nataleia Orlovska <nataleia.orlovska@gmail.com>
2023-03-09 21:36:43 +0200

    draft 2

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import mimetypes
 import threading
 import json
 import socket
+from threading import Thread
 
 HTTP_SERVER = '127.0.0.1'
 HTTP_PORT = 3000
@@ -15,12 +16,7 @@ SOCKET_PORT = 5000
 
 class MyApp(BaseHTTPRequestHandler):
     def do_POST(self):
-        data = self.rfile.read(int(self.headers['Content-Length']))
-        print(data)
-        data_parse = urllib.parse.unquote_plus(data.decode())
-        print(data_parse)
-        data_dict = {key: value for key, value in [el.split('=') for el in data_parse.split('&')]}
-        print(data_dict)
+
         self.send_response(302)
         self.send_header('Location', '/')
         self.end_headers()
@@ -30,10 +26,13 @@ class MyApp(BaseHTTPRequestHandler):
         pr_url = urllib.parse.urlparse(self.path)
         if pr_url.path == '/':
             self.send_html_file('index.html')
-        elif pr_url.path == '/contact':
+        elif pr_url.path == '/message.html':
             self.send_html_file('message.html')
         else:
-            self.send_html_file('error.html', 404)
+            if pathlib.Path().joinpath(pr_url.path[1:]).exists():
+                self.send_static()
+            else:
+                self.send_html_file('error.html', 404)
 
     def send_html_file(self, filename, status=200):
         self.send_response(status)
@@ -42,20 +41,20 @@ class MyApp(BaseHTTPRequestHandler):
         with open(filename, 'rb') as fd:
             self.wfile.write(fd.read())
 
-    def send_static(self, filename, status_code =200):
-        self.send_response(200)
-        mt = mimetypes.guess_type(self.path)
+    def send_static(self, filename, status_code = 200):
+        self.send_response(status_code)
+        mt = mimetypes.guess_type(filename)
         if mt:
             self.send_header("Content-type", mt[0])
         else:
             self.send_header("Content-type", 'text/plain')
         self.end_headers()
-        with open(f'.{self.path}', 'rb') as file:
+        with open(filename, 'rb') as file:
             self.wfile.write(file.read())
 
 
 
-def run():
+def http_server_run():
     server_address = ('localhost', 3000)
     http = HTTPServer(server_address, MyApp)
     try:
@@ -64,6 +63,15 @@ def run():
         logging.info("Server spoted!")
         http.server_close()
 
+def socket_server_run(host, port):
+    pass
+
 
 if __name__ == '__main__':
     run()
+    logging.basicConfig(level=logging.DEBUG, format = )
+    thread_server = Thread(target=http_server_run)
+    thread_server.start()
+
+    thread_socket_server = Thread(target=socket_server_run)
+    thread_socket_server.start()
