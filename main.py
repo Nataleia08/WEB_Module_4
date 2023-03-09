ef57ea3ac4e48506bfafecae4a7699156eb09501
Nataleia Orlovska <nataleia.orlovska@gmail.com>
2023-03-09 20:52:14 +0200

    draft 1

---
+++ main.py
@@ -0,0 +1,69 @@
+import logging
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import urllib.parse
+import pathlib
+import mimetypes
+import threading
+import json
+import socket
+
+HTTP_SERVER = '127.0.0.1'
+HTTP_PORT = 3000
+SOCKET_SERVER = '127.0.0.1'
+SOCKET_PORT = 5000
+
+
+class MyApp(BaseHTTPRequestHandler):
+    def do_POST(self):
+        data = self.rfile.read(int(self.headers['Content-Length']))
+        print(data)
+        data_parse = urllib.parse.unquote_plus(data.decode())
+        print(data_parse)
+        data_dict = {key: value for key, value in [el.split('=') for el in data_parse.split('&')]}
+        print(data_dict)
+        self.send_response(302)
+        self.send_header('Location', '/')
+        self.end_headers()
+
+
+    def do_GET(self):
+        pr_url = urllib.parse.urlparse(self.path)
+        if pr_url.path == '/':
+            self.send_html_file('index.html')
+        elif pr_url.path == '/contact':
+            self.send_html_file('message.html')
+        else:
+            self.send_html_file('error.html', 404)
+
+    def send_html_file(self, filename, status=200):
+        self.send_response(status)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        with open(filename, 'rb') as fd:
+            self.wfile.write(fd.read())
+
+    def send_static(self, filename, status_code =200):
+        self.send_response(200)
+        mt = mimetypes.guess_type(self.path)
+        if mt:
+            self.send_header("Content-type", mt[0])
+        else:
+            self.send_header("Content-type", 'text/plain')
+        self.end_headers()
+        with open(f'.{self.path}', 'rb') as file:
+            self.wfile.write(file.read())
+
+
+
+def run():
+    server_address = ('localhost', 3000)
+    http = HTTPServer(server_address, MyApp)
+    try:
+        http.serve_forever()
+    except KeyboardInterrupt:
+        logging.info("Server spoted!")
+        http.server_close()
+
+
+if __name__ == '__main__':
+    run()
